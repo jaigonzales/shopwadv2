d4c14d0d8ccdbee46f579e645ba037537ab48a3f
jgmaniegodev <jgmaniego.dev@gmail.com>
2017-09-15 13:06:05 +0800

    initial

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,6 @@ const  mix  = require('laravel-mix');
  |
  */
 
-mix.options({
-    extractVueStyles: false
-});
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
